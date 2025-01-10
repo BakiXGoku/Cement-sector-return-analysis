9aecf460d2e7c0b5b0c4b1243a8c3b60c671f35a
BakiXGoku <136944684+BakiXGoku@users.noreply.github.com>
2025-01-10 19:07:40 +0530

    Update correlation.py

--- Scripts/correlation.py
+++ Scripts/correlation.py
@@ -2,7 +2,9 @@ import pandas as pd
 
 nifty_data = pd.read_csv("NIFTY50.csv")
 sp_data = pd.read_csv("S&P 500.csv")
-sp_data['Date'] = pd.to_datetime(sp_data['Date'], format='%d-%m-%Y')
+sp_data['Date'] = pd.to_datetime(sp_data['Date'], format='mixed')
+
+
 
 # Convert Date column to datetime and remove time info
 nifty_data['Date'] = pd.to_datetime(nifty_data['Date']).dt.date
