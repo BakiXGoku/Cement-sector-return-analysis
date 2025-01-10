9d432e1d83f2edb6db2869078ad29cb3b02211c6
BakiXGoku <136944684+BakiXGoku@users.noreply.github.com>
2025-01-10 19:09:13 +0530

    Update correlation.py

--- Scripts/correlation.py
+++ Scripts/correlation.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
-nifty_data = pd.read_csv("NIFTY50.csv")
-sp_data = pd.read_csv("S&P 500.csv")
+nifty_data = pd.read_csv("D:\\MI\\trial\\starter\\sp500-nifty50-analysis\\\Data\\NIFTY50.csv")
+sp_data = pd.read_csv("D:\\MI\\trial\\starter\\sp500-nifty50-analysis\\Data\\S&P 500.csv")
 sp_data['Date'] = pd.to_datetime(sp_data['Date'], format='mixed')
 
 
